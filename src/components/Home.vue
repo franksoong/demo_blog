f6bed03957c0f7301fd6feba1692547bd57e23c4
soong@home@shi6w391 <songxinkun2003@sina.com>
2017-11-28 22:57:00 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,31 +5,32 @@
                 <b-navbar type="light" variant="light">
 
                     <b-navbar-brand href="#">
-                        <img class="fa fa-users brand" alt="Logo@Me">
+                        <img class="fa fa-users brand" alt="  Logo@Me">
                     </b-navbar-brand>
 
-                    <b-navbar-nav>
-                        <b-nav-item href="#">Link1</b-nav-item>
-                        <b-nav-item href="#">Link2</b-nav-item>
+                    <b-navbar-nav pills >
+                        <b-nav-item href="#">Home</b-nav-item>
+                        <b-nav-item href="#">Projects</b-nav-item>
+                        <b-nav-item href="#">About</b-nav-item>
+                        <b-nav-item href="#">Contact</b-nav-item>
                     </b-navbar-nav>
 
                     <!-- Right aligned nav items -->
-                    <b-navbar-nav class="ml-auto">
-
-                        <b-nav-item-dropdown text="Lang" right>
-                            <b-dropdown-item href="#">EN</b-dropdown-item>
-                            <b-dropdown-item href="#">ES</b-dropdown-item>
-                            <b-dropdown-item href="#">RU</b-dropdown-item>
-                            <b-dropdown-item href="#">FA</b-dropdown-item>
+                    <b-navbar-nav pills  class="ml-auto">
+                        <b-nav-item-dropdown text="Theme" right @mouseover="">
+                            <b-dropdown-item href="#">Light</b-dropdown-item>
+                            <b-dropdown-item href="#">Primary</b-dropdown-item>
+                            <b-dropdown-item href="#">Dark</b-dropdown-item>
+                            <b-dropdown-item href="#">Chrome</b-dropdown-item>
                         </b-nav-item-dropdown>
 
-                        <b-nav-item-dropdown right>
+                        <b-nav-item-dropdown text="Language" right>
                             <!-- Using button-content slot -->
-                            <template slot="button-content">
-                                <em>User</em>
-                            </template>
-                            <b-dropdown-item href="#">Profile</b-dropdown-item>
-                            <b-dropdown-item href="#">Signout</b-dropdown-item>
+                            <!-- <template slot="button-content">
+                                <em>Language</em>
+                            </template> -->
+                            <b-dropdown-item href="#">中文</b-dropdown-item>
+                            <b-dropdown-item href="#">Enlish</b-dropdown-item>
                         </b-nav-item-dropdown>
                     </b-navbar-nav>
                 </b-navbar>
@@ -90,7 +91,7 @@ export default {
 
 
 
-@include media-breakpoint-up(md) {
+@include media-breakpoint-up(lg) {
     .headercontainer {
         width: 100%;
 
